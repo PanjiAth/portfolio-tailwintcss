8ac40edbdd470315d48aa200abdc6830ec526319
PanjiAth <panjicahyono77@gmail.com>
2022-05-22 21:26:23 +0700

    dark mode

--- dist/js/script.js
+++ dist/js/script.js
@@ -2,11 +2,18 @@
 window.onscroll = function(){
     const header = document.querySelector('header');
     const fixedNav = header.offsetTop;
+    const totop = document.querySelector('#to-top');
 
     if(window.pageYOffset > fixedNav){
         header.classList.add('navbar-fixed');
+        totop.classList.remove('hidden');
+        totop.classList.add('flex');
+
     }else{
         header.classList.remove('navbar-fixed');
+        totop.classList.remove('flex');
+        totop.classList.add('hidden');
+        
     }
 }
 
@@ -19,3 +26,34 @@ hamburger.addEventListener('click',function(){
     hamburger.classList.toggle('hamburger-active');
     navMenu.classList.toggle('hidden');
 });
+
+//klik diluar hamburger
+window.addEventListener('click',function(e){
+    if(e.target != hamburger && e.target != navMenu){
+        hamburger.classList.remove('hamburger-active');
+        navMenu.classList.add('hidden');
+    }
+});
+
+//Dark Mode
+const darkToggle = document.querySelector('#dark-toggle');
+const html = document.querySelector('html');
+
+darkToggle.addEventListener('click',function(){
+    if(darkToggle.checked){
+        html.classList.add('dark');
+        localStorage.theme = 'dark';
+    }else{
+        html.classList.remove('dark');
+        localStorage.theme = 'light';
+    }
+})
+
+// pindahkan posisi toggle sesuai mode
+
+// On page load or when changing themes, best to add inline in `head` to avoid FOUC
+if (localStorage.theme === 'dark' || (!('theme' in localStorage) && window.matchMedia('(prefers-color-scheme: dark)').matches)) {
+    darkToggle.checked = true;
+  } else {
+    darkToggle.checked = false;
+  }
